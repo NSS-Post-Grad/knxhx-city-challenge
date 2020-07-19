30b5f812e99c6148be6fccd6aec36400acf2d8e0
kwohl <ktwohl@gmail.com>
2020-07-19 03:00:54 -0500

    added nav bar, logo, and nav css. added main content margins

--- neighborhoodApp/urls.py
+++ neighborhoodApp/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('neighborhoods/<int:org_id>', neighborhood_details, name='neighborhood'),
     path('mailinglist/', mail_list, name='mailinglist'),
     path('neighborhood/form', neighborhood_form, name='neighborhood_form'),
+    path('logout/', logout_user, name='logout'),
 ]
 
--- neighborhoodApp/views/__init__.py
+++ neighborhoodApp/views/__init__.py
@@ -3,3 +3,4 @@ from .neighborhoods.form import neighborhood_form
 from .neighborhoods.list import neighborhood_list
 from .neighborhoods.details import neighborhood_details
 from .residents.mail_list import mail_list
+from .auth.logout import logout_user
