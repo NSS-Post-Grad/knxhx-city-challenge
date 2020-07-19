bc33b6d8d1dbb763a36fc0bfd0d77e8e9cdb6d31
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-07-18 22:02:07 -0500

    all searches are working by exact criteria match

--- neighborhoodApp/views/search.py
+++ neighborhoodApp/views/search.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from neighborhoodApp.models import Resident, Neighborhood_org
 
+@login_required
 def search(request):
 
     if request.method == 'GET':
@@ -18,7 +20,61 @@ def search(request):
                 }
 
             elif form_data['search'] == 'org_type':
-                pass
+                
+                neighborhoods = Neighborhood_org.objects.filter(org_type=form_data['search-criteria'])
+
+                context = {
+                    "neighborhoods": neighborhoods
+                }
+
+            elif form_data['search'] == 'neighborhood_name':
+                
+                neighborhoods = Neighborhood_org.objects.filter(name=form_data['search-criteria'])
+
+                context = {
+                    "neighborhoods": neighborhoods
+                }
+
+            elif form_data['search'] == 'day':
+                
+                neighborhoods = Neighborhood_org.objects.filter(day=form_data['search-criteria'])
+
+                context = {
+                    "neighborhoods": neighborhoods
+                }
+
+            elif form_data['search'] == 'active':
+                
+                neighborhoods = Neighborhood_org.objects.filter(is_active=True)
+
+                context = {
+                    "neighborhoods": neighborhoods
+                }
+
+            elif form_data['search'] == 'inactive':
+                
+                neighborhoods = Neighborhood_org.objects.filter(is_active=False)
+
+                context = {
+                    "neighborhoods": neighborhoods
+                }
+
+            elif form_data['search'] == 'resident_first_name':
+                
+                residents = Resident.objects.filter(first_name=form_data['search-criteria'])
+
+                context = {
+                    "residents": residents
+                }
+
+            elif form_data['search'] == 'resident_last_name':
+                
+                residents = Resident.objects.filter(last_name=form_data['search-criteria'])
+
+                context = {
+                    "residents": residents
+                }
+
             
             template = 'search_results.html'
 
