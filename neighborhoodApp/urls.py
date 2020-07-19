c2f35eb4aecc42e619f4734b53d0c47e668027a6
mKroogz <mattk932009@gmail.com>
2020-07-18 21:15:45 -0500

    made base list view of neighborhood orgs

--- neighborhoodApp/urls.py
+++ neighborhoodApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from django.conf.urls import include
+from .views import *
 
 
 app_name = 'neighborhoodApp'
 
 urlpatterns = [
-
+path('neighborhoods/', neighborhood_list, name='neighborhoods'),
 ]
 
---
+++ neighborhoodApp/views/__init__.py
@@ -0,0 +1,1 @@
+from .neighborhoods.list import neighborhood_list
