9c88aed5dd556c2bff26afd5da710ee960a47bcc
mKroogz <mattk932009@gmail.com>
2020-07-19 02:56:28 -0500

    all resident views other than add form

--- neighborhoodApp/urls.py
+++ neighborhoodApp/urls.py
@@ -7,8 +7,11 @@ app_name = 'neighborhoodApp'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('home/', home, name='home'),
     path('neighborhoods/', neighborhood_list, name='neighborhoods'),
     path('neighborhoods/<int:org_id>', neighborhood_details, name='neighborhood'),
+    path('residents/', resident_list, name='residents'),
+    path('residents/<int:res_id>', resident_details, name='resident'),
     path('mailinglist/', mail_list, name='mailinglist'),
     path('neighborhood/form', neighborhood_form, name='neighborhood_form'),
 ]
